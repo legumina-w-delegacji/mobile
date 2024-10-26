b5e775517139cbfd05ddb8ecec58432181170f68
Mateusz Jamróz <jamr.mat@gmail.com>
2024-10-26 11:57:44 +0200

    dynamic calculation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, ScrollView, StyleSheet, Text, TouchableOpacity, View, ActivityIndicator } from "react-native";
 import FontAwesome from '@expo/vector-icons/FontAwesome'
 import themeStyles from "../themes/themes";
@@ -6,13 +6,31 @@ import { useQuery } from "@apollo/client";
 import { GET_EVENTS } from "../api/Queries";
 import LightButton from "../components/buttons/LightButton";
 import * as Notifications from 'expo-notifications';
+import { calculateDistance, calculateTime } from "../utils/PointCalculator";
+import * as Location from "expo-location";
+
 
 const HomePage = ({ navigation }: any) => {
-  const { loading, error, data } = useQuery<EventResponse>(GET_EVENTS);
+  const {loading, error, data } = useQuery<EventResponse>(GET_EVENTS);
+  const [currentLocation, setCurrentLocation]: any = useState({ latitude: 0, longitude: 0 });
 
   if (loading) return <ActivityIndicator />;
-
   if (error) return <></>
+
+    const getLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+            console.log("Permission to access location was denied");
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setCurrentLocation(location.coords);
+    };
+
+    if(currentLocation.latitude === 0 && currentLocation.longitude === 0)
+        getLocation();
+
 
   return <View>
     <ScrollView style={{ padding: 20}}>
@@ -30,8 +48,9 @@ const HomePage = ({ navigation }: any) => {
             <View style={{ ...styles.item, width: '84%' }}>
               <Text style={{ fontSize: 18, fontWeight: 'bold' }} key={index}>{item.name}</Text>
               <View style={{ marginTop: 5, flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Text style={{ fontSize: 12 }}>5 km od ciebie</Text>
-                <Text style={{ fontSize: 12 }}>5 godzin temu</Text>
+                <Text style={{ fontSize: 12 }}>{(calculateDistance(data!.events[0].lat, data!.events[0].lng, currentLocation.latitude, currentLocation.longitude) / 1000).toFixed(2)} km
+                 od ciebie</Text>
+                <Text style={{ fontSize: 12 }}>{calculateTime(item.createdAt)} godzin temu</Text>
               </View>
             </View>
             <View style={{ marginLeft: 'auto', marginTop: 2 }}>
@@ -41,7 +60,7 @@ const HomePage = ({ navigation }: any) => {
         </View>
       ))}
 
-      <Text>Jeśli jesteś osobą potrzebabnouwdi8hawdeajw90ującą pmocy mozesz sgloscić problem</Text>
+      <Text style={styles.miniText}>Jeśli jesteś osobą potrzebującą pmocy mozesz zgłosić problem</Text>
       <LightButton title="Potrzebuje pomocy" onPress={() => {
         navigation.navigate('HelpNeeded');
       }} />
@@ -58,6 +77,12 @@ const styles = StyleSheet.create({
   },
   itemInfo: {
 
+  },
+  miniText: {
+    fontSize: 12,
+    color: 'gray',
+    textAlign: 'center',
+    marginVertical: 10
   }
 });
 
